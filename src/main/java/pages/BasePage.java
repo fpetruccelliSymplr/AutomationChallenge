6fa46012b66662161c03f460b0fb5574b3b7910a
Franco Petruccelli <fpetruccelli@symplr.com>
2022-05-18 12:28:31 -0300

    base page

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage {
+
+    protected static WebDriver driver;
+    private static WebDriverWait wait;
+
+    static {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        driver = new ChromeDriver(chromeOptions);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public BasePage(WebDriver driver) {
+        BasePage.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public static void navigateTo(String url) {
+        driver.get(url);
+    }
+}
